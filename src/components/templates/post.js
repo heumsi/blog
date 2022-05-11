5cecdbb77ad2a7cea23dcd09c3eaa8ca0ef77880
heumsi <heumsi@naver.com>
2022-05-11 16:14:58 +0900

    feat: ToC를 추가한다

--- src/components/templates/post.js
+++ src/components/templates/post.js
@@ -1,7 +1,9 @@
-import * as React from 'react'
+import React, {useEffect} from 'react';
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-import { Disqus } from 'gatsby-plugin-disqus';
+import { Disqus } from 'gatsby-plugin-disqus'
+import * as tocbot from 'tocbot'
 import Layout from '../organisms/layout'
+
 
 const Post = ({ url, id, title, date, content }) => {
   const disqusConfig = {
@@ -10,13 +12,29 @@ const Post = ({ url, id, title, date, content }) => {
     title: title,
   }
 
+  // ref: 
+  // - https://github.com/DaW888/toc-react/blob/master/src/App.js
+  // - https://gizanbeak.com/post/tocbot
+  useEffect(() => {
+    tocbot.init({
+      tocSelector: '.toc',
+      contentSelector: '.post-content',
+      headingSelector: 'h2, h3',
+      hasInnerContainers: true,
+    });
+    return () => tocbot.destroy()
+  },[])
+  
   return (
     <Layout>
       <h1 className="post-title">{title}</h1>
       <span className="post-date">{date}</span>
-      <MDXRenderer>
-        {content}
-      </MDXRenderer>
+      <div className='toc'></div>
+      <div className='post-content'>
+        <MDXRenderer>
+          {content}
+        </MDXRenderer>
+      </div>
       <Disqus
         config={disqusConfig}
       />
