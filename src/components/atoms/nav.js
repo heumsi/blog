afee314833aeb8d6e7cbb89523f356c0a413e248
heumsi <heumsi@naver.com>
2022-05-14 15:50:22 +0900

    feat: nav에 github, linkedin 링크를 추가한다

--- src/components/atoms/nav.js
+++ src/components/atoms/nav.js
@@ -4,24 +4,32 @@ import { Link } from 'gatsby'
 const Nav = () => {
   return (
     <nav className="nav">
-        <ul className="nav-links">
+      <ul className="nav-links">
+        <li className="nav-link-item">
+          <Link to="/" className="nav-link-text">
+            Home
+          </Link>
+        </li>
+        {/* <li className="nav-link-item">
+          <Link to="/about" className="nav-link-text">
+            About
+          </Link>
+        </li>  */}
           <li className="nav-link-item">
-            <Link to="/" className="nav-link-text">
-              Home
-            </Link>
-          </li>
-          {/* <li className="nav-link-item">
-            <Link to="/about" className="nav-link-text">
-              About
-            </Link>
-          </li>  */}
-            <li className="nav-link-item">
-            <Link to="/posts" className="nav-link-text">
-              Posts
-            </Link>
-          </li>
-        </ul>
-      </nav>
+          <Link to="/posts" className="nav-link-text">
+            Posts
+          </Link>
+        </li>
+      </ul>
+      <ul className='external-links'>
+        <li className="nav-link-item">
+          <a href='https://github.com/heumsi/' target='_blank' rel="noopener noreferrer">GitHub</a>
+        </li>
+        <li className="nav-link-item">
+          <a href='https://www.linkedin.com/in/siheum-jeon-04222a1b3/' target='_blank' rel="noopener noreferrer">LinkedIn</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
