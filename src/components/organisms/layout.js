ae3002182ee5e21b9f9e50b1279b75f78ef44eda
heumsi <heumsi@naver.com>
2022-12-17 15:27:57 +0900

    feat: self-hosting plausible 스크립트를 추가한다

--- src/components/organisms/layout.js
+++ src/components/organisms/layout.js
@@ -3,12 +3,15 @@ import Header from "../atoms/header"
 import Content from '../atoms/content'
 import Footer from '../atoms/footer'
 import "../styles/main.css"
-
+import {Helmet} from "react-helmet-async";
 
 
 const Layout = ({ children }) => {
   return (
     <div className="container">
+        <Helmet>
+          <script defer data-domain="heumsi.github.io/blog" src="http://heumsi.chickenkiller.com/js/script.js"></script>
+        </Helmet>
       <Header/>
       <Content>
         { children }
